75db15d81bceabc1695f1bc68042634dd5508ab3
Wei Zhang <iusenenow@gmail.com>
2021-03-17 20:39:18 +1100

    update

--- AJAX/server.js
+++ AJAX/server.js
@@ -12,6 +12,23 @@ app.get("/server", (req, res) => {
   res.send("HELLO AJAX!")
 })
 
+//针对 IE 缓存
+app.get("/ie", (req, res) => {
+  //设置响应头：设置允许跨域
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  //设置响应体
+  res.send("HELLO IE缓存!")
+})
+
+app.get("/delay", (req, res) => {
+  //设置响应头：设置允许跨域
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  //设置响应体
+  setTimeout(() => {
+    res.send("HELLO IE缓存!")
+  }, 3000)
+})
+
 //可以接受任意类型的请求
 app.all("/server", (req, res) => {
   //设置响应头：设置允许跨域
@@ -26,7 +43,7 @@ app.all("/json-server", (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Header', '*')
   //对对象进行转换，因为send方法只能发送字符串和buffer
-  const data = JSON.stringify({ id: 1, name: "Teresa" })
+  const data = JSON.stringify({ age: 11, name: "Teresa" })
   //设置响应体
   res.send(data)
 })
