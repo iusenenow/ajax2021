0069df13f8ec8e60a9c1bb89d92df9d5a377a64c
Wei Zhang <iusenenow@gmail.com>
2021-03-20 17:37:54 +1100

    update

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const app = express()
 
 //3.创建路由规则
 app.get("/", (req, res) => {
-  res.send("Hello From Express!")
+  console.log(__dirname);
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/contact", (req, res) => {
+  res.send("Contact me at: Teresa@gmail.com")
+})
+
+app.get("/about", (req, res) => {
+  res.send("My name is Teresa and I am 11.")
 })
 
 //4.监听端口启动服务
 app.listen(8000, () => {
-  console.log("Connected to server port 8000..");
+  console.log("Connected to server port 8000..")
 })
