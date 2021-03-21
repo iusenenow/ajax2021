c9ac30a3e96a400d13034b3109adb3972a65d9b8
Wei Zhang <iusenenow@gmail.com>
2021-03-21 11:29:23 +1100

    update

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const express = require("express")
 //2.创建应用对象
 const app = express()
 
+// parse application/json, basically parse incoming Request Object as a JSON Object 
+// app.use(json())
+// combines the 2 above, then you can parse incoming Request Object if object, with nested objects, or generally any type.
+app.use(express.urlencoded({ extended: true }))
+
 //3.创建路由规则
 app.get("/", (req, res) => {
-  console.log(__dirname);
   res.sendFile(__dirname + "/index.html")
 })
 
@@ -16,6 +20,22 @@ app.get("/contact", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.send("My name is Teresa and I am 11.")
+})
+
+app.post("/", (req, res) => {
+  const { num1, num2 } = req.body
+  const result = Number(num1) + Number(num2)
+  res.send(`<h1>The result of the calculation is ${result}.</h1>`)
+})
+
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", (req, res) => {
+  const { weight, height } = req.body
+  const bmi = parseFloat(weight) / parseFloat(height * height)
+  res.send(`<h1>Your BMI is ${bmi}.</h1>`)
 })
 
 //4.监听端口启动服务
